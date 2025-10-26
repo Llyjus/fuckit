e69c63960c7b38f2ae8115247a8bfa2f7ae8b85d
Llyjus <Llyjus@protonmail.com>
2025-10-26 01:20:18 +0100

    Add files via upload

---
+++ quick_sort.py
@@ -0,0 +1,43 @@
+from time import perf_counter
+import random
+
+def counter(function, *args):
+    startTime = perf_counter()
+    function(*args)
+    endTime = perf_counter()
+    time = endTime - startTime
+    return time
+
+def createList(exponent):
+    randomList = []
+    for i in range(10**exponent):
+        n = random.randint(1, 10000)
+        randomList.append(n)
+    return randomList
+
+def quickSort(arr):
+
+    if len(arr) <= 1:
+        return arr
+
+    midNum = arr[ len(arr) // 2 ]
+
+    leftList = []
+    rightList = []
+    midList = []
+
+    for i in arr:
+        if i < midNum:
+            leftList.append(i)
+        elif i > midNum:
+            rightList.append(i)
+        else:
+            midList.append(i)
+    
+    return quickSort(leftList) + midList + quickSort(rightList)
+
+
+if __name__ == '__main__':
+    for i in range(1,6):
+        sortList = createList(i)
+        print(counter(quickSort, sortList))
